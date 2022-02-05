1c0606cd9ee9b033ef0d1caa902064b6a4057304
decoricky <rikiya.shikida@gmail.com>
2022-02-05 11:24:52 +0900

    Studioモデル作成

--- bmonster/admin.py
+++ bmonster/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Studio
+
+
+class StudioModelAdmin(admin.ModelAdmin):
+    list_display = ('id', 'code', 'name', 'created_datetime', 'modified_datetime')
+    readonly_fields = ('created_datetime', 'modified_datetime')
+
+
+admin.site.register(Studio, StudioModelAdmin)
--- bmonster/models.py
+++ bmonster/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Studio(models.Model):
+    class Meta:
+        db_table = 'studio'
+
+    code = models.CharField(verbose_name='コード', max_length=4, unique=True, db_index=True)
+    name = models.CharField(verbose_name='スタジオ', max_length=16, unique=True, db_index=True)
+    created_datetime = models.DateTimeField(verbose_name='登録日時', auto_now_add=True)
+    modified_datetime = models.DateTimeField(verbose_name='変更日時', auto_now=True)
+
+    def __str__(self):
+        return f'{self.code}:{self.name}'
