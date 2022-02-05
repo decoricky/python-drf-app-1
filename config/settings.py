917f623847a87570cbd505239e12e394b4bcf1e6
decoricky <rikiya.shikida@gmail.com>
2022-02-05 11:47:54 +0900

    SECRET_KEYを.envから読み込み

--- config/settings.py
+++ config/settings.py
@@ -9,18 +9,24 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
+import environ
 import os.path
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# env
+env = environ.Env()
+environ.Env.read_env(str(BASE_DIR / '.env'))
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-fk=dmsbtowk#^nj5bpr6roy9#ap&**a=&!sj(#@4@zil88n-))'
+# SECRET_KEY = 'django-insecure-fk=dmsbtowk#^nj5bpr6roy9#ap&**a=&!sj(#@4@zil88n-))'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
