4cacff751e5efe3ccab7251095362a0a2b264f65
decoricky <rikiya.shikida@gmail.com>
2022-02-06 14:11:21 +0900

    リファクタ

--- bmonster/scraping.py
+++ bmonster/scraping.py
@@ -13,10 +13,18 @@ PAGE_PATH = "reserve/"
 URL = urllib.parse.urljoin(BASE_URL, PAGE_PATH)
 
 
-def get_schedule_by_studio(
-        studio_code: str, studio_name: str, now: datetime.datetime = datetime.datetime.now(JST)) -> List[dict]:
+class ScrapingItem:
+    def __init__(self, studio_name, start_time, performer_name, program_name):
+        self.studio_name = studio_name
+        self.start_time = start_time
+        self.performer_name = performer_name
+        self.program_name = program_name
 
-    result_list = []
+
+def get_schedule_by_studio(
+        studio_code: str, studio_name: str, now: datetime.datetime = datetime.datetime.now(JST)) -> List[ScrapingItem]:
+
+    result_list: List[ScrapingItem] = []
 
     # 日付が変わるタイミングは避ける（23:59~00:01）
     if datetime.time(hour=23, minute=59) <= now.time() or now.time() < datetime.time(hour=0, minute=1):
@@ -53,13 +61,13 @@ def get_schedule_by_studio(
                 if not performer or not program:
                     continue
 
-                result = {
-                    'studio_name': studio_name,
-                    'start_time': datetime.datetime(
+                result = ScrapingItem(
+                    studio_name=studio_name,
+                    start_time=datetime.datetime(
                         year=date.year, month=date.month, day=date.day, hour=hour, minute=minute, tzinfo=JST),
-                    'performer': performer,
-                    'program': program
-                }
+                    performer_name=performer,
+                    program_name=program
+                )
                 result_list.append(result)
 
         date += datetime.timedelta(days=1)
@@ -67,55 +75,47 @@ def get_schedule_by_studio(
     return result_list
 
 
-def parse_schedule_to_performer_and_program(schedule_list: List[dict]) -> tuple:
-    performer_set = set()
-    program_set = set()
+def parse_schedule_to_performer_and_program(schedule_list: List[ScrapingItem]) -> tuple:
+    performer_name_set = set()
+    program_name_set = set()
 
     for schedule in schedule_list:
-        performer = schedule['performer']
-        program = schedule['program']
+        performer_name_set.add(schedule.performer_name)
+        program_name_set.add((schedule.performer_name, schedule.program_name))
 
-        performer_set.add(performer)
-        program_set.add((performer, program))
-
-    return performer_set, program_set
+    return performer_name_set, program_name_set
 
 
 def update_data():
     query_set = Studio.objects.all()
-    performer_set = set()
-    program_set = set()
-    schedule_list = []
+    item_list: List[ScrapingItem] = []
     for studio in query_set:
         studio_code = studio.code
         studio_name = studio.name
-        schedule_list = get_schedule_by_studio(studio_code, studio_name)
-        performer_set, program_set = parse_schedule_to_performer_and_program(schedule_list)
+        item_list += get_schedule_by_studio(studio_code, studio_name)
 
-    for name in performer_set:
+    performer_name_set, program_name_set = parse_schedule_to_performer_and_program(item_list)
+
+    for name in performer_name_set:
         try:
             performer = Performer.objects.get(name=name)
         except Performer.DoesNotExist:
             performer = Performer(name=name)
         performer.save()
 
-    for item in program_set:
-        performer_name = item[0]
-        program_name = item[1]
-
+    for performer_name, program_name in program_name_set:
         performer = Performer.objects.get(name=performer_name)
-
         try:
             program = Program.objects.get(performer=performer, name=program_name)
         except Program.DoesNotExist:
             program = Program(performer=performer, name=program_name)
         program.save()
 
-    for item in schedule_list:
-        studio_name = item['studio_name']
-        start_time = item['start_time']
-        performer_name = item['performer']
-        program_name = item['program']
+    for item in item_list:
+        studio_name = item.studio_name
+        start_time = item.start_time
+        performer_name = item.performer_name
+        program_name = item.program_name
 
         studio = Studio.objects.get(name=studio_name)
         performer = Performer.objects.get(name=performer_name)
